8e99b084a52d2cc7fe1ad8bb27b256c9fad101f0
Jeroen Schmit <jeroenschmit@schmitbook.fritz.box>
2012-10-11 09:17:38 +0200

    First sketch

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Slim & Dapper</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <meta property="og:title" content="Slim &amp; Dapper"/>
+		<meta property="og:type" content="website"/>
+		<meta property="og:url" content="http://slimndap.com"/>
+		<meta property="og:image" content="http://slimndap.com/logo/index.php/logo/png/200/200/0/000000"/>
+		<meta property="og:site_name" content="Slim &amp; Dapper"/>
+		<meta property="og:description"
+		      content="Jeroen Schmit, webdesigner- & developer."/>
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/style.css">
+
+        <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+        <div class="introduction">
+        	<img src="http://slimndap.com/logo/index.php/logo/png/300/200/0/ef5d3d">
+        	<div class="badge">
+	        	<p>
+	        		<h2>Slim &amp; Dapper is Jeroen Schmit, webdesigner- en developer. </h2>
+	        		<p>Gespecialiseerd in lichtgewicht Wordpress of Codeigniter websites voor smartphones, tablets &eacute;n desktops.</p>
+	        	</p>
+		        <div class="follow">
+					<img src="http://www.gravatar.com/avatar/<?php echo md5( strtolower( trim( "jeroen@slimndap.com" ) ) );?>">
+		        	<a href="http://facebook.com/slimndap" class="icon-facebook">Facebook</a>
+		        	<a href="http://twitter.com/slimndap" class="icon-twitter">Twitter</a>
+		        	<a href="http://www.linkedin.com/company/slim-&-dapper" class="icon-linkedin">LinkedIn</a>
+		        </div>
+        	</div>
+        </div>
+        <div class="contact">
+	        <div class="mail">
+	        	<a href="mailto:jeroen@slimndap.com"><i class="icon-envelope"></i> jeroen@slimndap.com</a>
+	        </div>
+	        <div class="phone">
+	        	<a href="tel:+31626216228"><i class="icon-phone"></i> Bel me</a>
+	        </div>
+        </div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
