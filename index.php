28b8f9bbd8c9967404d149d88d5cacaf0ece7e2f
Jeroen Schmit <jeroen@slimndap.com>
2012-10-17 19:29:47 +0200

    Apple Touch icons

--- index.php
+++ index.php
@@ -18,46 +18,67 @@
 		<meta property="og:description"
 		      content="Jeroen Schmit, webdesigner- & developer."/>
 
+		<link rel="publisher" href="https://plus.google.com/115893737395232267748" />
+		
+
 		<link href='http://fonts.googleapis.com/css?family=Signika+Negative:300,600&text=bcdeghijlmnoprstvwACDEIJLMNOPRS01268@-.%26' rel='stylesheet' type='text/css'>
-
-        <link rel="stylesheet" href="css/normalize.min.css">
+		
+		<meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-title" content="Slimndap">
+        
         <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/font-awesome-slimndap.css">
-        <link rel="stylesheet" href="css/style.css">
+        <!--[if IE 7]>
+	        <link rel="stylesheet" href="css/font-awesome-ie7.css">
+	    <![endif]-->
 
-        <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+        <script src="js/vendor/modernizr.custom.69989.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
         <div class="introduction">
+        	<div class="floater">
         	<h1>Slim & Dapper</h1>
     		<h2>Jeroen Schmit</h2>
     		<h3>webdesigner- & developer</h3>
 	        <div class="follow">
 				<a class="avatar">&nbsp;</a>
-	        	<a href="http://facebook.com/slimndap" class="icon-facebook"><span>Facebook</span></a>
-	        	<a href="http://twitter.com/slimndap" class="icon-twitter"><span>Twitter</span></a>
-	        	<a href="http://www.linkedin.com/company/slim-&-dapper" class="icon-linkedin"><span>LinkedIn</span></a>
+	        	<a href="http://facebook.com/slimndap" class="icon-facebook" onClick="recordOutboundLink(this, 'Externe link', 'facebook');return false;"><span>Facebook</span></a>
+	        	<a href="http://twitter.com/slimndap" class="icon-twitter" onClick="recordOutboundLink(this, 'Externe link', 'twitter');return false;"><span>Twitter</span></a>
+	        	<a href="http://www.linkedin.com/in/jeroenschmit" class="icon-linkedin" onClick="recordOutboundLink(this, 'Externe link', 'linkedin');return false;"><span>LinkedIn</span></a>
 	        </div>
+        	</div>
         </div>
         <div class="contact">
+        	<!--
+	        <div class="vcard">
+	        	<a href="jeroen_schmit.vcf" onClick="recordOutboundLink(this, 'Externe link', 'vcard');return false;"><i  class="icon-download-alt"></i> <span>download</span></a>
+	        </div>
+	        -->
 	        <div class="mail">
-	        	<a href="mailto:jeroen@slimndap.com" class="icon-envelope"><span>jeroen@slimndap.com</span></a>
+	        	<a href="mailto:jeroen@slimndap.com" onClick="recordOutboundLink(this, 'Externe link', 'mail');return false;"><i class="icon-envelope"></i> <span>jeroen@slimndap.com</span></a>
 	        </div>
 	        <div class="phone">
-	        	<a href="tel:+31626216228" class="icon-phone"><span>06-26216228</span></a>
+	        	<div class="dial"><a href="tel:+31626216228" onClick="recordOutboundLink(this, 'Externe link', 'phone');return false;"><i class="icon-phone"></i> <span>06-26216228</span></a></div>
+	        	<div class="view"><i class="icon-phone"></i> <span>06-26216228</span></div>
 	        </div>
         </div>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
 
         <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            var _gaq=[['_setAccount','UA-35564515-1'],['_trackPageview']];
             (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
             g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
             s.parentNode.insertBefore(g,s)}(document,'script'));
+            
+			function recordOutboundLink(link, category, action) {
+				try {
+					var myTracker=_gat._getTrackerByName();
+					_gaq.push(['myTracker._trackEvent', category , action ]);
+					setTimeout('document.location = "' + link.href + '"', 100)
+				}catch(err){}
+			}
         </script>
     </body>
 </html>
